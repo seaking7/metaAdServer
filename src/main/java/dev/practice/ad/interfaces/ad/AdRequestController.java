99b47b38adf550e14014fccb55a099923cf1800a
taekyungKim <seaking7@gmail.com>
2022-07-12 17:23:36 +0900

    AdRequestController 수정

--- src/main/java/dev/practice/ad/interfaces/ad/AdRequestController.java
+++ src/main/java/dev/practice/ad/interfaces/ad/AdRequestController.java
@@ -4,25 +4,57 @@ import dev.practice.ad.application.ad.AdRequestFacade;
 import dev.practice.ad.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/partners")
+@RequestMapping("/meta/v1")
 public class AdRequestController {
     private final AdRequestFacade adRequestFacade;
 
-    @PostMapping
-    public CommonResponse registerPartner(@RequestBody @Valid AdRequestDto.RegisterRequest request) {
-        var command = request.toCommand();
-        var adRequestInfo = adRequestFacade.registerPartner(command);
-        var response = new AdRequestDto.RegisterResponse(adRequestInfo);
-        return CommonResponse.success(response);
+    @GetMapping("/init")
+    public String getInitAd(@ModelAttribute AdRequestDto.AdInit adInit){
+        log.info("init appId:{}, appKey:{}, macAddress:{}, sdkVersion:{}", adInit.getAppId(), adInit.getAppKey(), adInit.getMacAddress(), adInit.getSdkVersion());
+        String returnStr = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<init>\n" +
+                "    <uuid></uuid>\n" +
+                "    <token></token>\n" +
+                "    <viewabilityPolicy>\n" +
+                "        <MeasuringScreenAreaSize>0,0,1,1</MeasuringScreenAreaSize>\n" +
+                "        <MeasuringScreenArea>100</MeasuringScreenArea>\n" +
+                "        <MeasuringInterval>0.2</MeasuringInterval>\n" +
+                "        <PollingInterval>1</PollingInterval>\n" +
+                "        <PixelGrid>100</PixelGrid>\n" +
+                "        <VisibleArea>0.5</VisibleArea>\n" +
+                "        <OnScreenLength>0.33</OnScreenLength>\n" +
+                "        <SkewAngle>45</SkewAngle>\n" +
+                "        <ActiveTime>1</ActiveTime>\n" +
+                "    </viewabilityPolicy>\n" +
+                "</init>";
+        return returnStr;
     }
+
+    @GetMapping("/request")
+    public String getRequestAd(@ModelAttribute @Valid AdRequestDto.AdRequest request ){
+        log.info("request appId:{}, token:{}, width:{}, height:{}", request.getAppId(), request.getToken(), request.getWidth(), request.getHeight());
+        String returnStr = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<adsinfo>\n" +
+                "    <adMediaType>Image</adMediaType>\n" +
+                "    <adUrl>http://203.248.154.18/i20/ads/20220510110642_SB_CA_05_2.0_0510.jpg</adUrl>\n" +
+                "    <adsId>1000</adsId>\n" +
+                "</adsinfo>";
+        return returnStr;
+    }
+
+    @GetMapping("/report")
+    public String getReportAd(@ModelAttribute @Valid AdRequestDto.AdReport report){
+        log.info("report appId:{}, getAdMediaType:{}, getAdMediaType:{}", report.getAppId(), report.getAdMediaType(), report.getAdMediaType());
+        String returnStr = "OK";
+        return returnStr;
+    }
+
+
 }
--- src/main/java/dev/practice/ad/interfaces/ad/AdRequestDto.java
+++ src/main/java/dev/practice/ad/interfaces/ad/AdRequestDto.java
@@ -1,55 +1,67 @@
 package dev.practice.ad.interfaces.ad;
 
-import dev.practice.ad.domain.ad.AdRequest;
 import dev.practice.ad.domain.ad.AdRequestCommand;
 import dev.practice.ad.domain.ad.AdRequestInfo;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 public class AdRequestDto {
 
+
+    @Getter
+    @ToString
+    public static class AdInit {
+        @NotEmpty(message = "appId 는 필수값입니다")
+        private String appId;
+        private String appKey;
+        private String macAddress;
+        private String sdkVersion;
+    }
+
+
     @Getter
     @Setter
     @ToString
-    public static class RegisterRequest {
-        @NotEmpty(message = "partnerName 는 필수값입니다")
-        private String partnerName;
+    public static class AdRequest
+    {
+        @NotEmpty(message = "appId 는 필수값입니다")
+        private String appId;
+        @NotEmpty(message = "token 는 필수값입니다")
+        private String token;
+        private String tag;
+        private String adRole;
+        private String duplicatedNum;
+        private String adMediaType;
+        private String unit;
+        private String width;
+        private String height;
+        private String sdkVersion;
 
-        @NotEmpty(message = "businessNo 는 필수값입니다")
-        private String businessNo;
 
-        @Email(message = "email 형식에 맞아야 합니다")
-        @NotEmpty(message = "email 는 필수값입니다")
-        private String email;
-
-        public AdRequestCommand toCommand() {
-            return AdRequestCommand.builder()
-                    .partnerName(partnerName)
-                    .businessNo(businessNo)
-                    .email(email)
-                    .build();
-        }
+//        public AdRequestCommand toCommand() {
+//            return AdRequestCommand.builder()
+//                    .a(partnerName)
+//                    .businessNo(businessNo)
+//                    .email(email)
+//                    .build();
+//        }
     }
 
     @Getter
     @ToString
-    public static class RegisterResponse {
-        private final String partnerToken;
-        private final String partnerName;
-        private final String businessNo;
-        private final String email;
-        private final AdRequest.Status status;
+    public static class AdReport {
+        @NotEmpty(message = "appId 는 필수값입니다")
+        private String appId;
 
-        public RegisterResponse(AdRequestInfo adRequestInfo) {
-            this.partnerToken = adRequestInfo.getPartnerToken();
-            this.partnerName = adRequestInfo.getPartnerName();
-            this.businessNo = adRequestInfo.getBusinessNo();
-            this.email = adRequestInfo.getEmail();
-            this.status = adRequestInfo.getStatus();
-        }
+        @NotEmpty(message = "uuid 는 필수값입니다")
+        private String uuid;
+        private String adsId;
+        private String adMediaType;
+
     }
 }
