cf98715225fa9371cdf20dec4147d33edb00f5e3
taekyungKim <seaking7@gmail.com>
2022-10-06 09:34:23 +0900

    build.gradle : plain jar 파일 생성안되도록 test db 세팅수정

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -15,31 +15,15 @@ CREATE TABLE `m_ads` (
 
 INSERT INTO m_ads
 (id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(9, '11001', '코로나광고', 'Image', 700, 965, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/ad_corona.jpeg', 'ENABLE', '2022-08-01 00:08:19.539970000', '2022-08-01 00:08:19.540037000');
-INSERT INTO m_ads
-(id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(10, '11002', '사이다 광고', 'Image', 358, 500, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/cider.jpeg', 'ENABLE', '2022-08-04 00:08:43.718610000', '2022-08-04 00:08:43.718652000');
-INSERT INTO m_ads
-(id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(11, '11003', '중소기업 광고', 'Video', 497, 280, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/company_ads_pre.mp4', 'ENABLE', '2022-08-04 00:09:20.155659000', '2022-08-04 00:09:20.155750000');
-INSERT INTO m_ads
-(id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(12, '11004', '하나은행 광고', 'Image', 700, 965, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/hanabank.jpeg', 'ENABLE', '2022-08-04 00:09:39.454502000', '2022-08-04 00:09:39.454648000');
-INSERT INTO m_ads
-(id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(13, '11005', '우리은행 광고', 'Image', 400, 700, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/ilovewoori.jpeg', 'ENABLE', '2022-08-04 00:09:59.755156000', '2022-08-04 00:09:59.755375000');
-INSERT INTO m_ads
-(id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(14, '11006', 'KB리브 광고', 'Image', 600, 300, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/kb_liiv.jpeg', 'ENABLE', '2022-08-04 00:10:34.523183000', '2022-08-04 00:10:34.523360000');
-INSERT INTO m_ads
-(id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(15, '11007', 'KCC광고', 'Image', 1280, 720, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/kcc_ad.jpeg', 'ENABLE', '2022-08-04 00:10:56.816051000', '2022-08-04 00:10:56.816356000');
-INSERT INTO m_ads
-(id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(16, '11008', 'Uplus 광고1', 'Video', 22, 512, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/uplus_ads_pre.mp4', 'ENABLE', '2022-08-04 00:11:26.304514000', '2022-09-05 10:56:00.716316000');
-INSERT INTO m_ads
-(id, ads_id, ads_name, ads_type, ads_width, ads_height, material_url, status, created_at, updated_at)
-VALUES(37, '11013', '13', 'Image', 912, 29, '129', 'ENABLE', '2022-09-05 11:53:17.170152000', '2022-09-05 11:55:53.701375000');
+VALUES(9, '11001', '코로나광고', 'Image', 700, 965, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/ad_corona.jpeg', 'ENABLE', now(), now())
+, (10, '11002', '사이다 광고', 'Image', 358, 500, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/cider.jpeg', 'ENABLE', now(), now())
+, (11, '11003', '중소기업 광고', 'Video', 497, 280, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/company_ads_pre.mp4', 'ENABLE', now(), now())
+, (12, '11004', '하나은행 광고', 'Image', 700, 965, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/hanabank.jpeg', 'ENABLE', now(), now())
+, (13, '11005', '우리은행 광고', 'Image', 400, 700, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/ilovewoori.jpeg', 'ENABLE', now(), now())
+, (14, '11006', 'KB리브 광고', 'Image', 600, 300, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/kb_liiv.jpeg', 'ENABLE', now(), now())
+, (15, '11007', 'KCC광고', 'Image', 1280, 720, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/kcc_ad.jpeg', 'ENABLE', now(), now())
+, (16, '11008', 'Uplus 광고1', 'Video', 22, 512, 'https://taekyung7.s3.ap-northeast-2.amazonaws.com/ads_meterial/uplus_ads_pre.mp4', 'ENABLE', now(), now())
+, (37, '11013', '13', 'Image', 912, 29, '129', 'ENABLE', now(), now());
 
 drop table if exists m_app CASCADE;
 CREATE TABLE `m_app` (
@@ -55,7 +39,7 @@ CREATE TABLE `m_app` (
 
 INSERT INTO m_app
 (id, created_at, updated_at, app_id, app_key, app_name, status)
-VALUES(1, '2022-08-01 09:18:04.647744000', '2022-08-26 09:18:04.647906000', 'game1', 'app_IF2ZJv8OCTc0CsUP', '메타버스 게임앱', 'ENABLE');
+VALUES(1, now(), now(), 'game1', 'app_IF2ZJv8OCTc0CsUP', '메타버스 게임앱', 'ENABLE');
 
 drop table if exists m_strategy CASCADE;
 CREATE TABLE `m_strategy` (
@@ -86,7 +70,7 @@ CREATE TABLE `m_strategy` (
 
 INSERT INTO m_strategy
 (id, measuring_screen_areax, measuring_screen_areay, measuring_screen_area_width, measuring_screen_area_height, max_measuring_count, waiting_interval, measuring_interval, viewable_time_image, viewable_time_video, cool_down_time, viewing_angle, visible_area, pixel_grid, reference_screen_handheldx, reference_screen_handheldy, reference_screen_bigx, reference_screen_bigy, screen_relative_length, created_at, updated_at)
-VALUES(2, 0, 0, 1, 1, 100, '1', '0.2', '1', '2', 3, 55, '0.9', '100', 1024, 768, 1536, 864, '0.1', '2022-09-01 08:21:51.326649000', '2022-09-05 08:22:39.019845000');
+VALUES(2, 0, 0, 1, 1, 100, '1', '0.2', '1', '2', 3, 55, '0.9', '100', 1024, 768, 1536, 864, '0.1', now(), now());
 
 drop table if exists m_user CASCADE;
 CREATE TABLE `m_user` (
