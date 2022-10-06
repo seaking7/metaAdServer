d92c97fdaa15ece546df65c378649d502647ec6c
taekyungKim <seaking7@gmail.com>
2022-10-06 14:10:28 +0900

    PID 파일 생성되도록 수정

--- src/main/java/dev/practice/ad/MetaAdServerApplication.java
+++ src/main/java/dev/practice/ad/MetaAdServerApplication.java
@@ -2,12 +2,15 @@ package dev.practice.ad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class MetaAdServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MetaAdServerApplication.class, args);
+        SpringApplication app = new SpringApplication(MetaAdServerApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
     }
 
 }
