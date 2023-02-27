d865407e0b5ee464749f8600c0eca9232c586dbd
taekyungKim <seaking7@gmail.com>
2023-02-27 16:16:25 +0900

    feat: tomcat ajp 통신 허용

--- src/main/java/dev/practice/ad/config/WebConfig.java
+++ src/main/java/dev/practice/ad/config/WebConfig.java
@@ -2,6 +2,13 @@ package dev.practice.ad.config;
 
 import dev.practice.ad.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,8 +20,37 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
+    @Value("${tomcat.ajp.port}")
+    private int ajpPort;
+
+    @Value("${tomcat.ajp.protocol}")
+    private String ajpProtocol;
+
+    @Value("${tomcat.ajp.address}")
+    private String ajpAddress;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createAjpConnector());
+        return tomcat;
+    }
+
+    private Connector createAjpConnector() {
+        Connector ajpConnector = new Connector(ajpProtocol);
+        ajpConnector.setPort(ajpPort);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setProperty("address", ajpAddress);
+        ajpConnector.setProperty("allowedRequestAttributesPattern", ".*");
+        ajpConnector.setScheme("http");
+        ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+        return ajpConnector;
     }
 }
